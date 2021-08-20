7cd1e6c5e59ed92111f066ea28356c50eed0d4f6
Hendrik Wagenseil <hendrik.wagenseil@metergroup.com>
2021-08-20 08:32:04 +0200

    some attempts to extract metadata

--- R/metadata_jpg.R
+++ R/metadata_jpg.R
@@ -7,14 +7,23 @@ library(magick)
 library(sf)
 
 # install exiftool: https://exiftool.org/install.html#Unix
-# see https://github.com/JoshOBrien/exiftoolr
+# github: https://github.com/JoshOBrien/exiftoolr
+# stackoverflow: https://stackoverflow.com/questions/59707871/extracting-metadata-from-large-set-of-images
+# https://superuser.com/questions/1370420/how-to-find-geolocation-of-a-photo-in-linux
 
 # list all jpgs
 fls = list.files("data/jpg/2_6c_Baumumfang/"
                  , pattern = "\\.jpg"
                  , full.names = TRUE)
 
-meta = exif_read(fls[1])
-dt  = meta[["CreateDate"]]
-lon = meta[["GPSLongitude"]]
-lat = meta[["GPSLatitude"]]
+# base
+file.info(fls[1])
+
+# magick
+img = image_read(fls[1])
+image_attributes(img)
+
+# exif
+meta = exif_read(fls[1], tags = "*GPS*")
+grep("File", names(meta), value = TRUE)
+exif_call(args = c("-n", "-j", "-q", "-*GPS*"), path = fls[1])
