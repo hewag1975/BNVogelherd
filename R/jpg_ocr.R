188b08f129503ac91c4e9627f8e18cf8f08a110e
Hendrik Wagenseil <hendrik.wagenseil@metergroup.com>
2021-08-22 10:07:08 +0200

    next attempt on ocr

--- R/jpg_ocr.R
+++ R/jpg_ocr.R
@@ -11,23 +11,24 @@ fls = list.files("image/2_6c_Baumumfang/"
                  , full.names = TRUE)
 
 ## ocr using magick ----
-img = image_read(fls[1])
-# image_attributes(img)
-# image_display(img)
-
-# attempt to extract timestamps printed onto the images
 mdata = exif_read(fls, tags = "*Image*")
 
-i = 8
+i = 16
 
 image_read(fls[i]) |> 
     # resize due to different original extents
     image_resize(geometry = "922x2000") |> 
     # extract area of timestamp
-    image_crop(geometry = "300x50+610+1925") |> 
+    # image_crop(geometry = "300x50+610+1925") |> 
+    image_crop(geometry = "100x50+800+1925") |> 
     image_convert(colorspace = "gray") |> 
-    image_convolve('DoG:0,0,2') |> 
     image_negate() |> 
+    image_normalize() |> 
+    image_quantize(max = 64) |> 
+    
+    
+    image_convolve('DoG:0,0,2') |> 
+    # image_negate() |> 
     image_threshold(type = "black", threshold = "80%") |> 
     image_ocr(language = "eng") 
 
@@ -51,15 +52,20 @@ v_ocr = lapply(l_ocr, \(i) {
         i = gsub("\\\n|()[[:punct:]]|()[a-z]|()[A-Z]", " ", i)
         i = gsub("\\s+", " ", i)
         i = trimws(i, which = "both")
-        # i = as.POSIXct(i, format = "%Y %m %d %H %M")
-        # i = as.character(i)
         return(i)
     })
 }) |> 
     unlist()
 
 datetime = data.table(file = basename(fls)
-                      , datetime = v_ocr)
+                      , dt_ocr = v_ocr)
+
+datetime[, dt := dt_ocr]
+datetime[, dt := gsub("202117122", "2021 7 22", dt)]
+datetime[, dt := gsub("202177122", "2021 7 22", dt)]
+datetime[, dt := gsub("22 0 16", "22 10 16", dt)]
+datetime[, dt := gsub("7122", "7 22", dt)]
+datetime[, dt := as.POSIXct(dt, format = "%Y %m %d %H %M")]
 
 datetime |> 
     readr::write_excel_csv2(file = "data/jpg_ocr.csv"
