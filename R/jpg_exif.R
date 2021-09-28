51b480c94eff7ba406458e7160121f71d733cb32
hendrik wagenseil <hendrik.wagenseil@metergroup.com>
2021-09-28 18:40:09 +0200

    extract x/y from images

--- R/jpg_exif.R
+++ R/jpg_exif.R
@@ -12,27 +12,34 @@ library(sf)
 # https://superuser.com/questions/1370420/how-to-find-geolocation-of-a-photo-in-linux
 
 # list all jpgs
-fls = list.files("image/2_6c_Baumumfang/"
-                 , pattern = "\\.jpg"
-                 , full.names = TRUE)
+pathimg = "data/img/Baumumfang"
+pathimg = "data/img/2_6c_Baumumfang"
+fls = list.files(pathimg, pattern = "\\.jpg", full.names = TRUE)
 
 ## approach 1: base R ----
-file.info(fls[1])
+# file.info(fls[1])
 
 ## approach 2: magick ----
-img = image_read(fls[1])
-image_attributes(img)
+# img = image_read(fls[1])
+# image_attributes(img)
 
 ## approach 3: exif ----
-meta = exif_read(fls)
-# grep("File", names(meta), value = TRUE)
 # exif_call(args = c("-n", "-j", "-q", "-*GPS*"), path = fls[1])
+meta = exif_read(fls) |> 
+    as.data.table()
 
-vars = c(grep("^File|^Image", names(meta), value = TRUE)
+vars = c(grep("^File|^GPS", names(meta), value = TRUE)
          , "Megapixels")
 
 meta |> 
     subset(select = vars) |> 
-    readr::write_excel_csv2(file = "data/jpg_exif.csv"
+    readr::write_excel_csv2(file = paste0(pathimg, "_exif.csv")
                             , na = ""
                             , col_names = TRUE)
+
+meta |> 
+    subset(select = vars) |> 
+    subset(!is.na(GPSLongitude)) |> 
+    st_as_sf(coords = c("GPSLongitude", "GPSLatitude"), crs = 4326) |> 
+    st_write(dsn = paste0(pathimg, ".shp"))
+
